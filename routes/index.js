7ac5213a995ffc4397bf4e755f0e55d2d99bde39
VortexNZ <42858473+gabebnz@users.noreply.github.com>
2022-02-01 21:30:06 +1300

    Minor splash UI changes
    Fixed minor css values for index page
    CSS created for signup page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Band Hub' });
+  res.render('index', { title: 'BandHub' });
+});
+
+router.get('/signup', function(req, res, next) {
+  res.render('signup', { title: 'BandHub | Signup' });
 });
 
 module.exports = router;
